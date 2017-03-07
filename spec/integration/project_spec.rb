8152aad1af63bffabe753c7a408a1537a18d5593
vhtien <tien.vo@gooddata.com>
2017-03-07 14:42:50 +0700

    test

--- spec/integration/project_spec.rb
+++ spec/integration/project_spec.rb
@@ -65,9 +65,9 @@ describe GoodData::Project, :constraint => 'slow' do
 
   describe '#import_users' do
     it 'should add user which login contains a part of whitelists' do
-      users = [ProjectHelper.create_random_user(@client, login: "hello+#{rand(1e7)}+admin@gooddata.com")]
+      users = [ProjectHelper.create_random_user(@client, login: "#{rand(1e7)}+rubydev+admin@gooddata.com")]
       @domain.create_users(users)
-      @project.import_users(users, domain: @domain, whitelists: [/admin@gooddata.com/])
+      @project.import_users(users, domain: @domain, whitelists: ['rubydev+admin@gooddata.com'])
       expect(@domain.members?(users)).to be_truthy
       expect(@project.members?(users)).to be_truthy
       expect(@project.members.count).to eq 2
